476d5da0aa1a86ec552a416e6a918915775cd2d4
Troy Washington <troyvw96@gmail.com>
2024-09-06 06:50:41 -0700

    custom bootstrap v5 legend

--- src/Form1.jsx
+++ src/Form1.jsx
@@ -6,8 +6,8 @@ function Form1() {
 
             <h1>Common HTML Form Elements</h1>
 
-            <fieldset >
-                <legend>User Login/ Your Information</legend>
+            <fieldset className='border rounded-3 p-3' >
+                <legend className="float-none w-auto px-3">User Login/ Your Information</legend>
                 <div>
                     <label for="username">Username:</label><br />
                     <input type="text" id="username" name="username" /><br />
