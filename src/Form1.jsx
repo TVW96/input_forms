deb7106463a2b5972abe06aa3c8dee2a52773ef2
Troy Washington <troyvw96@gmail.com>
2024-09-06 06:23:08 -0700

    addition

--- src/Form1.jsx
+++ src/Form1.jsx
@@ -4,12 +4,13 @@ function Form1() {
     return (
         <div >
             <h1>Common HTML Form</h1>
-            <form >
+            <fieldset >
+                <legend>User Login/ Your Information</legend>
                 <label for="username">Username:</label><br />
                 <input type="text" id="username" name="username" /><br />
                 <label for="pwd">Password:</label><br />
                 <input type="password" id="pwd" name="pwd" />
-            </form>
+            </fieldset>
         </div>
     )
 }
